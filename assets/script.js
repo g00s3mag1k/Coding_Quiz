037fcd413f17203c39bfd1773ebae747efe839f2
Brandon <beegee54321@gmail.com>
2023-10-27 18:12:38 -0700

    started variables in js some styling and added html in files

---
+++ assets/script.js
@@ -0,0 +1,8 @@
+var buttonStart = //start button
+var timeEl = //time
+var questionEl = //questions
+var questionLength = //how many questions 
+var userInput = //4 multiple choices
+var initialEl = //user initials
+var scoreEl = //scoreboard
+
